47661aa75e748423f416a0de34a533c4029a811b
Pjus <wnstjd117@naver.com>
2024-12-02 22:15:38 +0900

    chart zoom fix

--- stock_app/lib/screens/stock_detail_page.dart
+++ stock_app/lib/screens/stock_detail_page.dart
@@ -17,6 +17,9 @@ class StockDetailPage extends StatefulWidget {
 
 class _StockDetailPageState extends State<StockDetailPage> {
   Map<String, dynamic>? stockData;
+  final ScrollController _scrollController = ScrollController();
+  bool _disableScroll = false;
+  bool _isPointerInsideChart = false; // 차트 내부 상태 추적
   bool isLoading = true;
 
   @override
@@ -69,6 +72,21 @@ class _StockDetailPageState extends State<StockDetailPage> {
     }).toList();
   }
 
+  void _handlePointerSignal(PointerSignalEvent event) {
+    if (event is PointerScrollEvent && _isPointerInsideChart) {
+      // 차트 영역 내부에서만 줌 동작 처리
+      setState(() {
+        _disableScroll = true;
+      });
+      print('Zooming with scrollDelta: ${event.scrollDelta}');
+    } else {
+      // 차트 영역 밖에서 스크롤 복구
+      setState(() {
+        _disableScroll = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final candleData = _getCandleData();
@@ -80,61 +98,91 @@ class _StockDetailPageState extends State<StockDetailPage> {
       body: isLoading
           ? const Center(child: CircularProgressIndicator())
           : stockData != null
-              ? SingleChildScrollView(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        StockInfoWidget(stockData: stockData!),
-                        const SizedBox(height: 20),
-                        const Text(
-                          'Candlestick Chart',
-                          style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(height: 10),
-                        SizedBox(
-                          height: 500,
-                          child: InteractiveChart(
-                            candles: candleData,
-                            style: ChartStyle(
-                              priceGainColor: Colors.green,
-                              priceLossColor: Colors.red,
-                              volumeColor: Colors.blue.withOpacity(0.5),
-                              trendLineStyles: [
-                                Paint()
-                                  ..strokeWidth = 2.0
-                                  ..strokeCap = StrokeCap.round
-                                  ..color = Colors.orange,
-                              ],
-                              selectionHighlightColor:
-                                  Colors.blue.withOpacity(0.2),
-                              overlayBackgroundColor:
-                                  Colors.black.withOpacity(0.7),
-                              overlayTextStyle: const TextStyle(
-                                  color: Colors.white, fontSize: 12),
-                              volumeHeightFactor: 0.2,
-                            ),
-                            overlayInfo: (candle) => {
-                              "Open": "${candle.open?.toStringAsFixed(2)}",
-                              "High": "${candle.high?.toStringAsFixed(2)}",
-                              "Low": "${candle.low?.toStringAsFixed(2)}",
-                              "Close": "${candle.close?.toStringAsFixed(2)}",
-                              "Volume": "${candle.volume?.toStringAsFixed(2)}",
-                            },
+              ? NotificationListener(
+                  onNotification: (PointerSignalEvent event) {
+                    _handlePointerSignal(event); // 모든 PointerSignalEvent 감지
+                    return true;
+                  },
+                  child: SingleChildScrollView(
+                    controller: _scrollController,
+                    physics: _disableScroll
+                        ? const NeverScrollableScrollPhysics() // 스크롤 비활성화
+                        : const BouncingScrollPhysics(), //
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          StockInfoWidget(stockData: stockData!),
+                          const SizedBox(height: 20),
+                          const Text(
+                            'Candlestick Chart',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
                           ),
-                        ),
-                        const SizedBox(height: 20),
-                        const Divider(),
-                        const Text(
-                          'Additional Information',
-                          style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(height: 10),
-                        AdditionalInfoWidget(stockData: stockData!),
-                      ],
+                          const SizedBox(height: 10),
+                          MouseRegion(
+                            onEnter: (_) {
+                              setState(() {
+                                _isPointerInsideChart = true;
+                              });
+                            },
+                            onExit: (_) {
+                              setState(() {
+                                _isPointerInsideChart = false;
+                                _disableScroll = false; // 차트 외부로 나가면 스크롤 복구
+                              });
+                            },
+                            child: Listener(
+                              onPointerSignal: _handlePointerSignal,
+                              child: SizedBox(
+                                height: 500,
+                                child: InteractiveChart(
+                                  candles: candleData,
+                                  style: ChartStyle(
+                                    priceGainColor: Colors.green,
+                                    priceLossColor: Colors.red,
+                                    volumeColor: Colors.blue.withOpacity(0.5),
+                                    trendLineStyles: [
+                                      Paint()
+                                        ..strokeWidth = 2.0
+                                        ..strokeCap = StrokeCap.round
+                                        ..color = Colors.orange,
+                                    ],
+                                    selectionHighlightColor:
+                                        Colors.blue.withOpacity(0.2),
+                                    overlayBackgroundColor:
+                                        Colors.black.withOpacity(0.7),
+                                    overlayTextStyle: const TextStyle(
+                                        color: Colors.white, fontSize: 12),
+                                    volumeHeightFactor: 0.2,
+                                  ),
+                                  overlayInfo: (candle) => {
+                                    "Open":
+                                        "${candle.open?.toStringAsFixed(2)}",
+                                    "High":
+                                        "${candle.high?.toStringAsFixed(2)}",
+                                    "Low": "${candle.low?.toStringAsFixed(2)}",
+                                    "Close":
+                                        "${candle.close?.toStringAsFixed(2)}",
+                                    "Volume":
+                                        "${candle.volume?.toStringAsFixed(2)}",
+                                  },
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          const Divider(),
+                          const Text(
+                            'Additional Information',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 10),
+                          AdditionalInfoWidget(stockData: stockData!),
+                        ],
+                      ),
                     ),
                   ),
                 )
